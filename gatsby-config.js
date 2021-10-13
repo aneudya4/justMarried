a84e2b7a590c81b2d9f376800204c9fcc833e4ff
Aneudy Adames <a4aneudy@gmail.com>
2021-10-13 01:33:01 -0400

    t

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,27 +13,41 @@ module.exports = {
     description: "Just Married servicios para bodas en todo NY",
     author: "@AneudyA4",
     titleTemplate: "%s | Just Married",
-    url: "",
+    url: "https://demo-justmarried.netlify.app",
     twitterUsername: "@AneudyA4",
     image: `/main.png`
   },
 
-  plugins: [
-    {
-      resolve: "gatsby-plugin-anchor-links",
-      options: {
-        offset: 0,
-        duration: 300
+  plugins: [{
+    resolve: 'gatsby-plugin-robots-txt',
+    options: {
+      host: 'https://demo-justmarried.netlify.app',
+      sitemap: '',
+      env: {
+        development: {
+          policy: [{ userAgent: '*', disallow: ['/'] }]
+        },
+        production: {
+          policy: [{ userAgent: '*', allow: '/' }]
+        }
       }
+    }
+  },
+  {
+    resolve: "gatsby-plugin-anchor-links",
+    options: {
+      offset: 0,
+      duration: 300
+    }
+  },
+  {
+    resolve: `gatsby-source-contentful`,
+    options: {
+      spaceId: `hvuvunxxnk3l`,
+      // Learn about environment variables: https://gatsby.dev/env-vars
+      accessToken: process.env.CONTENTFUL_API_KEY,
     },
-    {
-      resolve: `gatsby-source-contentful`,
-      options: {
-        spaceId: `hvuvunxxnk3l`,
-        // Learn about environment variables: https://gatsby.dev/env-vars
-        accessToken: process.env.CONTENTFUL_API_KEY,
-      },
-    },
+  },
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`, // Needed for dynamic images
