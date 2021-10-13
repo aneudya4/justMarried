a56f1a7137702655917db1bda9f7346099bf30ea
Aneudy Adames <a4aneudy@gmail.com>
2021-10-13 10:49:37 -0400

    testing pluging web manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,7 @@ module.exports = {
       background_color: `#f5eae6`,
       theme_color: `#f5eae6`,
       display: `standalone`,
-      icon: `src/static/main.png`, // This path is relative to the root of the site.
+      icon: `/main.png`, // This path is relative to the root of the site.
     },
 
 
