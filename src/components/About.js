40c4bbbe0f3419a3efe2189d8f3c6710f853a7bc
Aneudy Adames <a4aneudy@gmail.com>
2021-11-04 02:10:34 -0400

    added some new styling to the about section , also  updated the keywords meta tag

--- src/components/About.js
+++ src/components/About.js
@@ -1,30 +1,40 @@
-import React from 'react'
+import React from "react"
 // import img from '../images/lis-about-1.jpeg'
 // import lis from '../images/lis-people.jpeg'
 // import couple from '../images/service-1.jpg'
-import { StaticImage } from 'gatsby-plugin-image'
+import { StaticImage } from "gatsby-plugin-image"
 
 export default function About() {
-    return (
-        <section className='about container' id='about'>
-            <h2 className='main-title'>Just Married</h2>
-            <div className='about__details'>
-                <h3>Una Ceremonia Inolvidable</h3>
-                <div className='about__imgs flex-container'>
-                    <div className='about__main__img'>
-                        <StaticImage src='../images/Lissette.jpeg' alt='wedding' placeholder='blurred' />
-                    </div>
-                    <div className='about__side__imgs flex-container'>
-                        {/* <img src={lis} alt='about' /> */}
-                        <StaticImage src='../images/lis-about-1.jpeg' alt='wedding' placeholder='blurred'
-                        />
-                        <StaticImage src='../images/service-1.jpg' alt='wedding' placeholder='blurred'
-                        />
-
-
-                    </div>
-                </div>
+  return (
+    <section className="about container" id="about">
+      <h2 className="main-title">Just Married</h2>
+      <div className="about__details">
+        <h3>Una Ceremonia Inolvidable</h3>
+        <div className="about__imgs flex-container">
+          <div className="about__main__img">
+            <StaticImage
+              src="../images/lisette.jpeg"
+              alt="wedding"
+              placeholder="blurred"
+            />
+            <div className="about__main-title">
+              <h2>Oficiante de bodas Lissette Marte </h2>
             </div>
-        </section>
-    )
+          </div>
+          <div className="about__side__imgs flex-container">
+            <StaticImage
+              src="../images/lis-about-1.jpeg"
+              alt="wedding"
+              placeholder="blurred"
+            />
+            <StaticImage
+              src="../images/service-1.jpg"
+              alt="wedding"
+              placeholder="blurred"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
