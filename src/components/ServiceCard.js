f3014469a652a551397c6c56b2ff17de35a8de9d
Aneudy Adames <a4aneudy@gmail.com>
2021-11-04 20:35:21 -0400

    added optimizations for images

--- src/components/ServiceCard.js
+++ src/components/ServiceCard.js
@@ -1,19 +1,22 @@
-import React from 'react'
-import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+import React from "react"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 export default function ServiceCard({ ceremony }) {
-
-    return (
-        <div className='services__item'>
-            <div className='services__item-image'>
-                <GatsbyImage image={getImage(ceremony.serviceImage)} alt={ceremony.serviceTitle} placeholder='blurred' width={350}
-                    height={240} />
-            </div>
-            <div className='services__item-content'>
-                <h3 className='services__item-title'>{ceremony.serviceTitle}</h3>
-                <p className='services__item-description'>
-                    {ceremony.serviceDesc}
-                </p>
-            </div>
-        </div>
-    )
+  return (
+    <div className="services__item">
+      <div className="services__item-image">
+        <GatsbyImage
+          image={getImage(ceremony.serviceImage)}
+          alt={ceremony.serviceTitle}
+          placeholder="blurred"
+          width={350}
+          height={350}
+          layout="fixed"
+        />
+      </div>
+      <div className="services__item-content">
+        <h3 className="services__item-title">{ceremony.serviceTitle}</h3>
+        <p className="services__item-description">{ceremony.serviceDesc}</p>
+      </div>
+    </div>
+  )
 }
