959fb646025f2d610c514ca9e0c8e2626e29ef2c
Aneudy Adames <a4aneudy@gmail.com>
2021-11-05 10:29:20 -0400

    added a scrollable nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,30 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import Navigation from "./Navigation"
 import MobileMenu from "./MobileNav"
+import ScrollableNav from "./ScrollableNav"
 
 export default function Header() {
+  const [scroll, setScroll] = useState(false)
+
+  const changeBackground = () => {
+    setScroll(window.scrollY >= 50)
+  }
+
+  useEffect(() => {
+    let mounted = true
+    if (mounted) {
+      window.addEventListener("scroll", changeBackground)
+    }
+
+    return function cleanup() {
+      mounted = false
+      window.removeEventListener("scroll", changeBackground)
+    }
+  }, [])
   return (
     <header className="container">
       <Navigation />
+      {scroll && <ScrollableNav />}
       <MobileMenu />
     </header>
   )
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Header from "./Header"
 import Footer from "../components/Footer"
+
 import "../pages/main.css"
 export default function Layout({ children }) {
   return (
