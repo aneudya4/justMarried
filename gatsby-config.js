36c8c205087efd8f3316a98060bd4024bacbf9f0
Aneudy Adames <a4aneudy@gmail.com>
2022-01-28 21:16:34 -0500

    changed the site url and added the twitter to the page

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,72 +7,72 @@ require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 
-module.exports = {
+https: module.exports = {
   siteMetadata: {
     title: "Just Married",
     description: "Just Married oficiante de bodas en todo New York",
-    author: "@AneudyA4",
+    author: "Aneudy Adames",
     titleTemplate: "%s | Just Married",
-    url: "https://demo-justmarried.netlify.app",
-    twitterUsername: "@AneudyA4",
-    siteUrl: "https://demo-justmarried.netlify.app",
-    image: `/main.png`
+    url: "https://just-married.us",
+    twitterUsername: "@Lismarte17",
+    siteUrl: "https://just-married.us",
+    image: `/main.png`,
   },
 
+  plugins: [
+    {
+      resolve: "gatsby-plugin-robots-txt",
 
-  plugins: [{
-    resolve: 'gatsby-plugin-robots-txt',
-
-    options: {
-      host: 'https://demo-justmarried.netlify.app',
-      sitemap: '',
-      env: {
-        development: {
-          policy: [{ userAgent: '*', disallow: ['/'] }]
+      options: {
+        host: "https://just-married.us",
+        sitemap: "",
+        env: {
+          development: {
+            policy: [{ userAgent: "*", disallow: ["/"] }],
+          },
+          production: {
+            policy: [{ userAgent: "*", allow: "/" }],
+          },
         },
-        production: {
-          policy: [{ userAgent: '*', allow: '/' }]
-        }
-      }
+      },
     },
-  },
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`, // Needed for dynamic images
     `gatsby-plugin-react-helmet`, // Needed for SEO,
-    'gatsby-plugin-sitemap',
-  {
-    resolve: "gatsby-plugin-anchor-links",
-    options: {
-      offset: 0,
-      duration: 300
-    }
-  },
-  {
-    resolve: `gatsby-source-contentful`,
-    options: {
-      spaceId: `57uzuhp84xhf`,
-      // Learn about environment variables: https://gatsby.dev/env-vars
-      accessToken: process.env.CONTENTFUL_API_KEY,
-    },
-
-  }, {
-    resolve: `gatsby-plugin-manifest`,
-    options: {
-      name: `Just Married  Wedding Service`,
-      short_name: `Just Married`,
-      icon: `src/images/icon.png`,
-      start_url: `/`,
-      background_color: `#f5eae6`,
-      theme_color: `#f5eae6`,
-      display: `standalone`,
-      icon_options: {
-        // For all the options available,
-        // please see the section "Additional Resources" below.
-        purpose: `any maskable`,
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-anchor-links",
+      options: {
+        offset: 0,
+        duration: 300,
       },
     },
-
-  }, `gatsby-plugin-offline`
-  ]
-};
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `57uzuhp84xhf`,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_API_KEY,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Just Married  Wedding Service`,
+        short_name: `Just Married`,
+        icon: `src/images/icon.png`,
+        start_url: `/`,
+        background_color: `#f5eae6`,
+        theme_color: `#f5eae6`,
+        display: `standalone`,
+        icon_options: {
+          // For all the options available,
+          // please see the section "Additional Resources" below.
+          purpose: `any maskable`,
+        },
+      },
+    },
+    `gatsby-plugin-offline`,
+  ],
+}
